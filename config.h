da7708322fdb4f69774ad6744c29760fbe85d3a8
nagi12147 <nagi12147@tutanota.com>
2022-01-01 17:59:55 +0700

    bigger font size

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "monospace:pixelsize=15:antialias=true:autohint=true";
+static char *font = "monospace:pixelsize=17:antialias=true";
 static int borderpx = 2;
 
 /*
